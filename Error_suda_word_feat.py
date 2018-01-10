96c6fd01fda558b7f76d5a005c85d0e8744e4f1b
bamtercelboo <bamtercelboo@163.com>
2018-01-10 13:14:29 +0800

    update demo of calculate error

--- Error_suda_word_feat.py
+++ Error_suda_word_feat.py
@@ -2,9 +2,9 @@
 
 import random
 import os
+import sys
 import numpy as np
 import math
-import tqdm
 import multiprocessing as mu
 
 
@@ -13,15 +13,21 @@ def read_word_feat(path_word_vector, path_feat_vector):
     word_vec = {}
     word_list = []
     print("reading feat vectors from file......")
+    now_line = 0
     for line in open(path_feat_vector, encoding="UTF-8"):
+        now_line += 1
         feat_vec[line.strip().split()[0]] = line.strip().split()[1:]
-    print("Finished")
+        sys.stdout.write("\rHandling with the {} line".format(now_line))
+    print("\nFinished")
 
     print("reading word vectors from file......")
+    now_line = 0
     for line in open(path_word_vector, encoding="UTF-8"):
+        now_line += 1
         word_vec[line.strip().split()[0]] = line.strip().split()[1:]
         word_list.append(line.strip().split()[0])
-    print("Finished")
+        sys.stdout.write("\rHandling with the {} line".format(now_line))
+    print("\nFinished")
     return word_list, word_vec, feat_vec
 
 
@@ -33,12 +39,14 @@ def handle_word(word, word_vec):
 
 
 def handle_feat(word, feat_vec):
+    # print(word)
     vector = 0
     feat_count = 0
     word = "<" + word + ">"
     for feat_num in range(3, 7):
         for i in range(0, len(word) - feat_num + 1):
-            feat = "S@" + str(feat_num) + "#" + word[i:(i + feat_num)]
+            # feat = "S@" + str(feat_num) + "#" + word[i:(i + feat_num)]
+            feat = word[i:(i + feat_num)]
             if feat.strip() in feat_vec:
                 # print(feat.strip(), feat_vec[feat.strip()])
                 feat_count += 1
@@ -55,9 +63,10 @@ def handle_feat(word, feat_vec):
 def cal_error(sample_number, sample_k, word_vec, feat_vec):
     print("calculate error......")
     euclidean_avg = []
-    sample_number_tqdm = tqdm.trange(sample_number)
-    for sample_num in sample_number_tqdm:
-        sample_number_tqdm.set_description("Process:" + str(sample_k))
+    # sample_number_tqdm = tqdm.trange(sample_number)
+    for sample_num in range(sample_number):
+        sys.stdout.write("\rHandling with the {}/{} sample in {}".format(sample_num, sample_number, sample_k))
+        # sample_number_tqdm.set_description("Process:" + str(sample_k))
         sample_word_list = random.sample(word_list, sample_k)
         # print(sample_word_list)
         # sample_word_list_tqdm = tqdm.tqdm(sample_word_list)
@@ -82,25 +91,25 @@ def cal_error(sample_number, sample_k, word_vec, feat_vec):
 
 
 if __name__ == "__main__":
-    # path_word_vector = "./glove.6B.100d.txt"
-    # path_feat_vector = "./eng.feat.model.small"
-    path_word_vector = "/home/lzl/mszhang/subword/subword/eng.word.model"
-    path_feat_vector = "/home/lzl/mszhang/subword/subword/eng.feat.model"
-    path_result = "./result_subword_1.txt"
+    path_word_vector = "./enwiki.emb.source.small"
+    path_feat_vector = "./enwiki.emb.feature.small"
+    # path_word_vector = "/data/mszhang/ACL2017-Word2Vec/experiments-v0/subword/enwiki.emb.source"
+    # path_feat_vector = "/data/mszhang/ACL2017-Word2Vec/experiments-v0/subword/enwiki.emb.feature"
+    path_result = "./suda_error_result_subword.txt"
     word_list, word_vec, feat_vec = read_word_feat(path_word_vector=path_word_vector, path_feat_vector=path_feat_vector)
 
     if os.path.exists(path_result):
         os.remove(path_result)
 
     list_sample_k = [100, 500, 1000, 3000, 5000, 8000, 10000]
-    list_sample_k_tqdm = tqdm.tqdm(list_sample_k)
+    # list_sample_k_tqdm = tqdm.tqdm(list_sample_k)
     file = open(path_result, mode="w", buffering=1)
     file.writelines(["sample_number", " ", "sample_k", " ", "error_avg", "\n"])
-    for sample_k in list_sample_k_tqdm:
-        list_sample_k_tqdm.set_description("Processing:")
+    for sample_k in list_sample_k:
+        # list_sample_k_tqdm.set_description("Processing:")
         error_avg = cal_error(1000, int(sample_k), word_vec, feat_vec)
         file.writelines([str(1000), "    ", str(sample_k), "     ", str(error_avg.round(6)), "\n"])
-        print("The result is {}, {}, {}".format(1000, sample_k, error_avg.round(6)))
+        print("\nThe result is {}, {}, {}".format(1000, sample_k, error_avg.round(6)))
     file.close()
     
 
