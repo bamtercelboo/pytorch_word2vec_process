cff140a78a29b8f454cc42d579649f2e5f9bdc36
bamtercelboo <bamtercelboo@163.com>
2018-01-17 18:28:07 +0800

    add script file ro subword

--- script_for_sentence_classification/handle_subword_feat.py
+++ script_for_sentence_classification/handle_subword_feat.py
@@ -1,14 +1,13 @@
 # @Author : bamtercelboo
-# @Datetime : 2018/1/16 13:58
-# @File : handle_context.py
-# @Last Modify Time : 2018/1/16 13:58
+# @Datetime : 2018/1/17 18:16
+# @File : handle_subword_feat.py
+# @Last Modify Time : 2018/1/17 18:16
 # @Contact : bamtercelboo@{gmail.com, 163.com}
 
 """
-    FILE :  handle_subword.py
+    FILE :  handle_subword_feat.py
     FUNCTION : None
 """
-
 import re
 import sys
 import numpy as np
@@ -48,25 +47,6 @@ def read_data(path_data=None):
         # data = list(sorted(set(data_list), reverse=True))
         data = list(set(data_list))
     return data
-
-
-def read_source_embedding(path_sourceEmbedding=None):
-    print("read source embedding.......")
-    with open(path_sourceEmbedding, encoding="UTF-8") as f:
-        source_embedding_dict = {}
-        source_embedding_dim = 0
-        now_line = 0
-        for line in f.readlines():
-            now_line += 1
-            sys.stdout.write("\rreading {} line.".format(now_line))
-            line = line.strip().split(" ")
-            if len(line) == 1 or len(line) == 2:
-                continue
-            source_embedding_dim = len(line) - 1
-            source_embedding_dict[line[0]] = line[1:]
-    f.close()
-    print("\nread source embedding Finished")
-    return source_embedding_dict, source_embedding_dim
 
 
 def read_feat_embedding(path_featEmbedding=None):
@@ -112,8 +92,7 @@ def write_embed(file=None, word=None, word_embed=None):
     file.write("\n")
 
 
-def handle_Embedding(data_list=None, source_embedding_dict=None, feat_embedding_dict=None, embedding_dim=0,
-                     path_Save_wordEmbedding=None):
+def handle_Embedding(data_list=None, feat_embedding_dict=None, embedding_dim=0, path_Save_wordEmbedding=None):
     print("Handle Embedding......")
     file = open(path_Save_wordEmbedding, encoding="UTF-8", mode="w")
     file.write(str(embedding_dim) + "\n")
@@ -124,47 +103,32 @@ def handle_Embedding(data_list=None, source_embedding_dict=None, feat_embedding_
     for word in data_list:
         now_word += 1
         sys.stdout.write("\rhandling with the {} word in data_list, all {} words.".format(now_word, all_word))
-        # print(word)
-        if word in source_embedding_dict:
-            iov_num += 1
-            source_embedding_list = [float(i) for i in source_embed_dict[word]]
-            source_embedding = np.array(source_embedding_list)
-            feat_sum_embedding, feat_ngram_num = n_gram(word=word, feat_embedding_dict=feat_embedding_dict)
-            word_embed = (feat_sum_embedding + source_embedding) / (1 + feat_ngram_num)
-            write_embed(file=file, word=word, word_embed=word_embed)
-        else:
-            oov_num += 1
-            feat_sum_embedding, feat_ngram_num = n_gram(word=word, feat_embedding_dict=feat_embedding_dict)
-            if not isinstance(feat_sum_embedding, np.ndarray):
-                # if the word no n-gram in feature, replace with zero
-                feat_sum_embedding = np.array(list([0] * embedding_dim))
-                feat_ngram_num = 1
-            feat_sum_embedding = feat_sum_embedding / feat_ngram_num
-            write_embed(file=file, word=word, word_embed=feat_sum_embedding)
+        feat_sum_embedding, feat_ngram_num = n_gram(word=word, feat_embedding_dict=feat_embedding_dict)
+        if not isinstance(feat_sum_embedding, np.ndarray):
+            # if the word no n-gram in feature, replace with zero
+            feat_sum_embedding = np.array(list([0] * embedding_dim))
+            feat_ngram_num = 1
+        feat_sum_embedding = feat_sum_embedding / feat_ngram_num
+        write_embed(file=file, word=word, word_embed=feat_sum_embedding)
     file.close()
-    print("\niov number {} , oov number {}, all words {} == {}".format(iov_num, oov_num, (iov_num + oov_num),
-                                                                       len(data_list)))
-    print("Handle Embedding Finished")
+    print("\nHandle Embedding Finished")
 
 
 if __name__ == "__main__":
     # path_data = "./Data/CR/custrev.all"
     path_data = "./Data/MR/rt-polarity.all"
-    path_sourceEmbedding = "./embedding/subword.enwiki.emb.source.small"
     path_featEmbedding = "./embedding/subword.enwiki.emb.feature.small"
     path_Save_wordEmbedding = "./embedding/convert_subword_MR.txt"
 
     # path_data = "./Data/CR/custrev.all"
     # path_data = "./Data/MR/rt-polarity.all"
     # path_data = "./Data/Subj/subj.all"
-    # path_sourceEmbedding = "/home/lzl/mszhang/suda_file_0113/file/subword/enwiki.emb.source"
     # path_featEmbedding = "/home/lzl/mszhang/suda_file_0113/file/subword/enwiki.emb.feature"
     # path_Save_wordEmbedding = "/home/lzl/mszhang/suda_file_0113/file/context/sentence_classification/enwiki.emb.source_CR.txt"
 
     data_list = read_data(path_data=path_data)
-    # data_list = ["wayulink", "fileski", "promotioned", "asdasd"]
-    source_embed_dict, source_embed_dim = read_source_embedding(path_sourceEmbedding=path_sourceEmbedding)
     feat_embed_dict, feat_embed_dim = read_feat_embedding(path_featEmbedding=path_featEmbedding)
-    assert source_embed_dim == feat_embed_dim
-    handle_Embedding(data_list=data_list, source_embedding_dict=source_embed_dict, feat_embedding_dict=feat_embed_dict,
-                     embedding_dim=source_embed_dim, path_Save_wordEmbedding=path_Save_wordEmbedding)
+    handle_Embedding(data_list=data_list, feat_embedding_dict=feat_embed_dict,
+                     embedding_dim=feat_embed_dim, path_Save_wordEmbedding=path_Save_wordEmbedding)
+
+
